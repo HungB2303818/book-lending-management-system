956c3dab5ed8dc5aff0c66ceebdb804ffd130ea2
HungB2303818 <hungb2303818@student.ctu.edu.vn>
2025-10-31 12:46:14 +0700

    cai dat thu muc du an

---
+++ app.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const express = require("express");
+const cors = require("cors");
+const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "internal Server Error",
+  });
+});
+
+module.exports = app;
